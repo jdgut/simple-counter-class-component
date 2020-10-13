1525fa5c6f654e58bdd7b32de2b505b3eb1504fc
jd <jdgutierrez80@gmail.com>
2020-10-13 00:29:29 -0400

    Final version of class compontent

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-import Counter from "./component/Counter"
+import Counter from "./component/Counter";
 
 //include bootstrap npm library into the bundle
 import "bootstrap";
 
 //include your index.scss file into the bundle
-import "../styles/index.scss";
+//import "../styles/index.scss";
 
 //render your react application
 ReactDOM.render(<Counter />, document.querySelector("#app"));
